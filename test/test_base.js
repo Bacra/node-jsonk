84f60874f16fb1a9c54c60125bd05235b7cc6fb7
Bacra Woo <bacra.woo@gmail.com>
2016-09-04 12:10:59 +0800

    Add: test base

---
+++ test/test_base.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var expect = require('expect.js');
+var JSONK = require('../');
+
+describe('#base', function()
+{
+	it('#stringify', function()
+	{
+		var jk = new JSONK();
+		var data = {
+			string: 'string',
+			number: 11,
+			boolean: true,
+			nuLL: null,
+			undef: undefined,
+			array: ['string', 11, true, {number: 33}, undefined],
+			object: {number: 22}
+		};
+
+		expect(jk.stringify(data)).to.be.eql(data);
+
+		for(var i in data)
+		{
+			if (data.hasOwnProperty(i))
+			{
+				expect(jk.stringify(data[i])).to.be.eql(data[i]);
+			}
+		}
+	});
+});
