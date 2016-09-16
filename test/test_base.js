a6b7b9fee73a7b6891f9bde6946ae8f11a405d3d
Bacra Woo <bacra.woo@gmail.com>
2016-09-16 13:17:31 +0800

    Add: `parseNoWrap` method

--- test/test_base.js
+++ test/test_base.js
@@ -3,38 +3,95 @@
 var expect = require('expect.js');
 var jk = require('../');
 var JSONK = jk.JSONK;
-var baseData = {
-	string: 'string',
-	number: 11,
-	boolean: true,
-	nuLL: null,
-	undef: undefined,
-	array: ['string', 11, true, {number: 33}, undefined],
-	object: {number: 22}
-};
+
 
 describe('#base', function()
 {
-	it('#stringify', function()
+	describe('#base type', function()
 	{
-		expect(jk.stringify(baseData)).to.be.eql(
-			{
-				jsonk_data: baseData
-			});
+		var baseData = {
+			string: 'string',
+			number: 11,
+			boolean: true,
+			nuLL: null,
+			undef: undefined,
+			array: ['string', 11, true, {number: 33}, undefined],
+			object: {number: 22}
+		};
 
-		for(var i in baseData)
+		it('#stringify', function()
 		{
-			if (baseData.hasOwnProperty(i))
+			expect(jk.stringify(baseData)).to.be.eql(
+				{
+					jsonk_data: baseData
+				});
+
+			for(var i in baseData)
 			{
-				console.log(i, baseData[i]);
-				expect(jk.stringify(baseData[i]))
-					.to.be.eql(
-					{
-						jsonk_data: baseData[i]
-					});
+				if (baseData.hasOwnProperty(i))
+				{
+					expect(jk.stringify(baseData[i]))
+						.to.be.eql(
+						{
+							jsonk_data: baseData[i]
+						});
+				}
 			}
-		}
+		});
+
+		it('#parse', function()
+		{
+			expect(jk.parse({jsonk_data: baseData})).to.be.eql(baseData);
+
+			for(var i in baseData)
+			{
+				if (baseData.hasOwnProperty(i))
+				{
+					expect(jk.parse({jsonk_data: baseData[i]})).to.be.eql(baseData[i]);
+				}
+			}
+		});
+
 	});
+
+	describe('#use parse', function()
+	{
+		var now = Date.now();
+		var baseData = {
+			jsonk_data: {k: 'Date', v: now},
+			parsers: 'Date'
+		};
+
+		it('#base', function()
+		{
+			expect(jk.parse(baseData).getTime()).to.be(now);
+		});
+
+		it('#noparser', function()
+		{
+			delete baseData.parsers;
+			expect(jk.parse(baseData)).to.be.eql(baseData.jsonk_data);
+		});
+
+		it('#parseNoWrap', function()
+		{
+			expect(jk.parseNoWrap(baseData.jsonk_data).getTime()).to.be(now);
+		});
+
+		it('#outparser', function()
+		{
+			expect(function()
+				{
+					expect(jk.parse({jsonk_data: {}, parsers: 'not_exists_parser'}));
+				})
+				.to.throwError(function(e)
+				{
+					expect(e.message).to.be('Miss Parser,not_exists_parser');
+				});
+		});
+
+	});
+
 
 	it('#escape', function()
 	{
@@ -42,19 +99,6 @@ describe('#base', function()
 			.to.be.eql({jsonk_data: {k: '\\', v: {k: '\\', v: {}}}});
 	});
 
-
-	it('#parse', function()
-	{
-		expect(jk.parse({jsonk_data: baseData})).to.be.eql(baseData);
-
-		for(var i in baseData)
-		{
-			if (baseData.hasOwnProperty(i))
-			{
-				expect(jk.parse({jsonk_data: baseData[i]})).to.be.eql(baseData[i]);
-			}
-		}
-	});
 
 
 	it('#stringifyToString', function()
@@ -71,28 +115,5 @@ describe('#base', function()
 
 		expect(jk.parseFromString("{jsonk_data:{string: 'string'}}"))
 			.to.be.eql({string: 'string'});
-	});
-
-	describe('#stringify wrap info', function()
-	{
-		it('#noparser', function()
-		{
-			var now = Date.now();
-			expect(jk.parse({jsonk_data: {k: 'Date', v: now}}))
-				.to.be.eql({k: 'Date', v: now});
-		});
-
-
-		it('#outparser', function()
-		{
-			expect(function()
-				{
-					expect(jk.parse({jsonk_data: {}, parsers: 'not_exists_parser'}));
-				})
-				.to.throwError(function(e)
-				{
-					expect(e.message).to.be('Miss Parser,not_exists_parser');
-				});
-		});
 	});
 });
